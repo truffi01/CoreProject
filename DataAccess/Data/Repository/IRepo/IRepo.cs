f7bcc7f90c2cf24e4bcf02169def21abad064173
Thomas Ruffino <thomasruffino1@gmail.com>
2020-01-07 09:31:01 -0500

    Finished IRepo for Project

--- DataAccess/Data/Repository/IRepo/IRepo.cs
+++ DataAccess/Data/Repository/IRepo/IRepo.cs
@@ -1,7 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions; 
+
 namespace DataAccess.Data.Repository.IRepo
 {
-    public interface IRepo
+    public interface IRepo<T> where T : class 
     {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        void Add(T entity);
+
+        void Remove(int id);
+        void Remove(T entity); 
     }
 }
